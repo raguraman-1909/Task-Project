cddb31249809c5d5312833015737c6c9e4372e8f
Raguraman <124240325+raguraman-1909@users.noreply.github.com>
2023-12-18 18:00:31 +0530

    TaskService.java
    TaskService Implementation

--- Task Project/Task-Project/src/main/java/com/TaskProject/service/TaskService.java
+++ Task Project/Task-Project/src/main/java/com/TaskProject/service/TaskService.java
@@ -5,28 +5,30 @@ import com.TaskProject.model.User;
 
 import java.util.List;
 
-
+//Service interface for managing tasks in the system
 public interface TaskService {
 
+    //Creates a new task.
     void createTask(Task task);
 
+    // Updates an existing task.
     void updateTask(Long id, Task task);
 
-    void deleteTask(Long id);
+    void deleteTask(Long id);    //Deletes a task by its ID.
 
-    List<Task> findAll();
+    List<Task> findAll();    //Retrieves a list of all tasks in the system.
 
-    List<Task> findByOwnerOrderByDateDesc(User user);
+    List<Task> findByOwnerOrderByDateDesc(User user);    //Retrieves tasks owned by a user, ordered by date in descending order.
 
-    void setTaskCompleted(Long id);
+    void setTaskCompleted(Long id);    //Marks a task as completed.
 
-    void setTaskNotCompleted(Long id);
+    void setTaskNotCompleted(Long id);    //Marks a task as not completed.
+    
+    List<Task> findFreeTasks();    //Retrieves a list of tasks not assigned to any user.
 
-    List<Task> findFreeTasks();
+    Task getTaskById(Long taskId);    //Retrieves a task by its ID
 
-    Task getTaskById(Long taskId);
+    void assignTaskToUser(Task task, User user);    //Assigns a task to a user.
 
-    void assignTaskToUser(Task task, User user);
-
-    void unassignTask(Task task);
+    void unassignTask(Task task);    //Unassigns a task to a user.
 }
