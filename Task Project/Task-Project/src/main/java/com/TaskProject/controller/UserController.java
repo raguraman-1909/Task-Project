a2fa669ea125180d1f6da61df78235f5da414497
Raguraman <124240325+raguraman-1909@users.noreply.github.com>
2023-12-18 17:36:30 +0530

    UserController.java
    UserController

--- Task Project/Task-Project/src/main/java/com/TaskProject/controller/UserController.java
+++ Task Project/Task-Project/src/main/java/com/TaskProject/controller/UserController.java
@@ -10,10 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+//REST controller and MVC controller for handling user-related operations.
 @RestController
 @RequestMapping("/api/users")
-@Controller
 public class UserController {
 
     private final UserService userService;
@@ -23,6 +22,7 @@ public class UserController {
         this.userService = userService;
     }
 
+    //Retrieves a list of users and renders the user view.
     @GetMapping("/users")
     public String listUsers(Model model, SecurityContextHolderAwareRequestWrapper request) {
         boolean isAdminSigned = request.isUserInRole("ROLE_ADMIN");
@@ -32,6 +32,7 @@ public class UserController {
         return "views/tasks";
     }
 
+    //Deletes a user based on the provided user ID.
     @GetMapping("user/delete/{id}")
     public String deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
