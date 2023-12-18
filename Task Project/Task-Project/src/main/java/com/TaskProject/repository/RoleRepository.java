4a824fe3118a5cafdfc70c924b5fce3997876573
Raguraman <124240325+raguraman-1909@users.noreply.github.com>
2023-12-18 18:08:37 +0530

    RoleRepository.java
    Role repository

--- Task Project/Task-Project/src/main/java/com/TaskProject/repository/RoleRepository.java
+++ Task Project/Task-Project/src/main/java/com/TaskProject/repository/RoleRepository.java
@@ -3,9 +3,10 @@ package com.TaskProject.repository;
 import com.TaskProject.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Integer> {
 
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {    Repository interface for managing roles in the system.
+    //Jpa repsitory provides a set of generic CRUD (Create, Read, Update, Delete) operations for working with entities.
     Role findByRole(String user);
 }
 
