47ed2079cdfd0f3cb807e9fddd2f78b4a27d15ec
Raguraman <124240325+raguraman-1909@users.noreply.github.com>
2023-12-18 17:53:35 +0530

    RoleService.java
    RoleService Interface

--- Task Project/Task-Project/src/main/java/com/TaskProject/service/RoleService.java
+++ Task Project/Task-Project/src/main/java/com/TaskProject/service/RoleService.java
@@ -4,9 +4,11 @@ import com.TaskProject.model.Role;
 
 import java.util.List;
 
-
+//Service interface for managing roles in the system.
 public interface RoleService {
+    //to create a role
     Role createRole(Role role);
 
+    // to list all the role
     List<Role> findAll();
 }
